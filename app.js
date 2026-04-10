481b27533adaa6e4c9c16f035117101ceda46122
fox520-sketch <fox520486@gmail.com>
2026-04-10 08:31:23 +0800

    V13

--- app.js
+++ app.js
@@ -13,7 +13,7 @@
   const CENTER = TRACK_SIZE / 2;
   const DEFAULT_CANVAS_SCALE = 1;
   const DEFAULT_VIEW_SCALE = 1;
-  const MIN_VIEW_SCALE = 0.2;
+  const MIN_VIEW_SCALE = 0.01;
   const MAX_VIEW_SCALE = 4.8;
   const DEFAULT_WORLD_SCALE = 8;
 
